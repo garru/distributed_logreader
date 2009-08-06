afbf80bdbfe215e28b52086f297730455721f53e
Gary Tsang <gary@garru.com>
2009-08-06 16:25:05 -0700

    Added max queue size to keep memory usage reasonable

--- lib/distributed_logreader/distributer/simple_thread_pool.rb
+++ lib/distributed_logreader/distributer/simple_thread_pool.rb
@@ -2,18 +2,21 @@ require 'thread'
 
 module DLogReader
   class SimpleThreadPool
-    attr_accessor :num_threads, :worker, :thread_pool, :queue
-    
-    def initialize(worker, num_threads = 10)
+    attr_accessor :num_threads, :worker, :thread_pool, :queue, :max_queue_size
+    def initialize(worker, num_threads = 5)
       self.worker = worker
       self.num_threads = num_threads
       self.queue = Queue.new
+      self.max_queue_size = 100
       num_threads.times do 
         create_thread
       end
     end
     
     def process(line)
+      while(queue.size > self.max_queue_size)
+        sleep(0.1)
+      end
       self.queue << line
     end
     
