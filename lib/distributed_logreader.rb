067f5e5eb7fbacc678742d8775cfb99e08f2b9df
Gary Tsang <gary@garru.com>
2009-08-12 17:12:21 -0700

    a not distributed scribe logreader to test

--- lib/distributed_logreader.rb
+++ lib/distributed_logreader.rb
@@ -8,5 +8,5 @@ require 'distributed_logreader/distributed_log_reader/scribe_reader'
 require 'logger'
 
 $dlog_logger = Logger.new("/var/log/distributed_logreader.log")
-$dlog_logger.level = Logger::INFO
+$dlog_logger.level = Logger::DEBUG
 $dlog_logger.datetime_format = "%Y-%m-%d %H:%M:%S "
--- lib/distributed_logreader/distributed_log_reader/scribe_reader.rb
+++ lib/distributed_logreader/distributed_log_reader/scribe_reader.rb
@@ -1,14 +1,31 @@
 module DLogReader
-  class ScribeReader < DistributedLogReader
+  class ScribeReader
     attr_accessor :selector, :archiver
     attr_reader :log_reader
     def initialize(filename, backupdir, worker, num_threads = 10)
       super(filename, worker, num_threads)
       self.selector = RotatingLog.new
       self.selector.ignore_conditions << lambda{|x| !x.match(/scribe_stats/).nil?}
+      @worker = worker
       self.archiver = DateDir.new(backupdir)
     end
-        
+    
+    def process
+      $dlog_logger.info("Started  #{log_file}:")
+      lines_processed = 0
+      @log_reader = LogReader.new(log_file) do |line|
+        @worker.call(line)
+        lines_processed += 1
+      end
+      @log_reader.run
+      $dlog_logger.info("Finished #{log_file}: Processed (#{lines_processed}) lines")
+      post_process
+    end
+    
+    def log_file
+      self.filename
+    end
+    
     def log_file
       @log_file ||= begin
         selector.file_to_process(filename)
